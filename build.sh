c6faaa226806ccd2c3711d32dc7e7525806e3479
oleg_dixon <olegdixon5973@gmail.com>
2025-09-08 21:14:45 +0500

    Changes and fixes for deployment

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
+make install
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
-make install && make collectstatic && make migrate
+# make install && make collectstatic && make migrate
 # make install && psql -a -d $DATABASE_URL -f database.sql
