c3d792193c5a5ee5011176533cc15260ac575428
oleg_dixon <olegdixon5973@gmail.com>
2025-09-16 13:32:36 +0500

    Added a task filter to the template

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from .models import Task
 from task_manager.statuses.models import Status
+from task_manager.users.models import User
 from task_manager.tags.models import Tag
 
 User = get_user_model()
@@ -56,3 +57,35 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'tags']
+
+
+class TaskFilterForm(forms.Form):
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        required=False,
+        label='Статус',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label='Исполнитель',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    author = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label='Автор',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        required=False,
+        label='Метки',
+        widget=forms.SelectMultiple(attrs={'class': 'form-select'})
+    )
+    self_tasks = forms.BooleanField(
+        required=False,
+        label='Только мои задачи',
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -2,15 +2,44 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from .models import Task
-from .forms import TaskForm
-from task_manager.mixins import TaskPermissionMixin, ContextActionMixin
+from .forms import TaskForm, TaskFilterForm
+from task_manager.mixins import LoginRequiredMixin, TaskPermissionMixin, ContextActionMixin
 
 
-class TaskListView(ListView):
+class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'tasks/index.html'
     context_object_name = 'tasks'
     ordering = ['id']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        form = TaskFilterForm(self.request.GET)
+
+        if form.is_valid():
+            status = form.cleaned_data.get('status')
+            executor = form.cleaned_data.get('executor')
+            author = form.cleaned_data.get('author')
+            tags = form.cleaned_data.get('tags')
+            self_tasks = form.cleaned_data.get('self_tasks')
+
+            if status:
+                queryset = queryset.filter(status=status)
+            if executor:
+                queryset = queryset.filter(executor=executor)
+            if author:
+                queryset = queryset.filter(author=author)
+            if tags:
+                queryset = queryset.filter(tags__in=tags).distinct()
+            if self_tasks:
+                queryset = queryset.filter(author=self.request.user)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter_form'] = TaskFilterForm(self.request.GET or None)
+        return context
 
 
 class TaskCreateView(SuccessMessageMixin, ContextActionMixin, CreateView):
