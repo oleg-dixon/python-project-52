719e4624f8049c99d5a8e91572281b52720b7bcf
oleg_dixon <olegdixon5973@gmail.com>
2025-09-21 12:16:48 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
-import sys
+from django.conf import settings
+# import sys
 
 
 def main():
@@ -9,6 +10,14 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.conf import settings
+        import sys
+
+        # 🔍 DEBUG вывод для CI
+        print("=== DEBUG CI SETTINGS ===", file=sys.stderr)
+        print("BASE_DIR =", settings.BASE_DIR, file=sys.stderr)
+        print("TEMPLATES DIRS =", settings.TEMPLATES[0].get("DIRS"), file=sys.stderr)
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -16,6 +25,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 
 if __name__ == '__main__':
